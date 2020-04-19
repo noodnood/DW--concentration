6a75944d57e538f1a9866999f78140eab6ab36de
noodnood <62505510+noodnood@users.noreply.github.com>
2020-04-19 21:58:41 +0800

    Update concentration.py

--- concentration.py
+++ concentration.py
@@ -55,8 +55,6 @@ class Board():
         self.lineList = []
 
     def progress(self, match = False, rowNum=1, colNum=1):
-        # rowNum-=1
-        # colNum-=1
         newLine = ""
         sep = "  "
         newlineList = []
@@ -84,9 +82,11 @@ class Board():
                 newlineList.append(eachRow)
             self.lineList = newlineList
             
-    def show(self):
+    def show(self, end=False): #end = Boolean
         line = ""
         entry = "\'?\'"
+        if end == True:
+            entry = "\'0\'"
         lineList = []
 
         for col in range(self.cols):
@@ -104,10 +104,26 @@ class Board():
 
         self.lineList = lineList
 
+class Coordinate():
+    def __init__(self, deck_of_cards, board):
+        a = list(string.ascii_lowercase)
+        coordinates = input("Please enter a coordinate (a1,c4 etc):\n>>>") #of the form a1, b1 etc
+        selection = list(coordinates) # ['a', '1']  
+        row = int(selection[1]) 
+        row -= 1
+        col = int(a.index(selection[0]))
+        self.coordinates = coordinates # 'a1'
+        self.row = row
+        self.col = col        
+        if self.row < 0 or self.row >= board.rows or self.col >= board.cols:
+            self.allowed = False
+            print("Invalid input, please ensure your row number is between 1 and {} and your column number from a to {}".format(board.rows,a[board.cols-1]))
+        else:
+            self.allowed = True
+            self.name = deck_of_cards.deckGrid()[self.row, self.col] # e.g. 3 of Hearts
+
 class Game():
     def __init__(self):
-        self.selection1 = ''
-        self.selection2 = ''
         self.numrow1 = 0
         self.numcol1 = 0
         self.numrow2 = 0
@@ -115,9 +131,15 @@ class Game():
         self.match = False
         self.end = False
         self.memoryList = []
+        self.skip = False
+        self.coordinate1 = 0
+        self.coordinate2 = 0
 
     def startScreen(self):
         print("Welcome to Concentration!")
+
+    def endScreen(self, board):
+        board.show(self.end)
 
     def gameComplete(self, board):
         for line in board.lineList:
@@ -127,101 +149,63 @@ class Game():
                 else:
                     self.end = False
 
-    def makeSelection(self, deck_of_cards, board):
+    def Select(self, deck_of_cards, board): #this initializes 2 coordinates that you choose
         a = list(string.ascii_lowercase)
-        coordinate1 = input("Please enter a coordinate (a1,c4 etc):\n>>>") #of the form a1, b1 etc
-        self.coordinate1 = coordinate1
-        selection1 = list(coordinate1)
-        row1 = int(selection1[1])  
-        row1 -= 1
-        if row1 >= board.rows:
-            print("Invalid input, please ensure your row number is between 1 and {}".format(board.rows))
-            return self.makeSelection(deck_of_cards, board)
+        self.coordinate1 = Coordinate(deck_of_cards, board) # ['a', '1'] 
+        if self.coordinate1.allowed == False:
+            return self.Select(deck_of_cards, board)
+        self.selection1 = self.coordinate1.coordinates
+        self.numcol1 = self.coordinate1.col
+        self.numrow1 = self.coordinate1.row
+        print("You have selected the > {} <".format(self.coordinate1.name))
 
-        col1 = int(a.index(selection1[0]))
-        #selection1[1] = row1
-        self.selection1 = deck_of_cards.deckGrid()[row1, col1]
-        self.numcol1 = col1
-        self.numrow1 = row1
-        print("You have selected the > {} <".format(self.selection1))
-
-        coordinate2 = input("Please enter a coordinate (a1,c4 etc):\n>>>") #of the form a1, b1 etc
-        self.coordinate2 = coordinate2
-        selection2 = list(coordinate2)
-        row2 = int(selection2[1])
-        row2 -= 1
-        if row2 >= board.rows:
-            print("Invalid input, please ensure your row number is between 1 and {}".format(board.rows))
-            return self.makeSelection(deck_of_cards, board)
-
-        col2 = int(a.index(selection2[0]))
-        #selection2[1] = row2
-        self.selection2 = deck_of_cards.deckGrid()[row2, col2]
-        self.numcol2 =col2
-        self.numrow2 = row2
-        print("You have selected the > {} <".format(self.selection2))
+        self.coordinate2 = Coordinate(deck_of_cards, board)
+        if self.coordinate2.allowed == False:
+            return self.Select(deck_of_cards, board)
+        self.selection2 = self.coordinate2.coordinates
+        self.numcol2 = self.coordinate2.col
+        self.numrow2 = self.coordinate2.row
+        print("You have selected the > {} <".format(self.coordinate2.name))
         print("\n")
-        print("Your selections were: the {} and the {}.".format(self.selection1, self.selection2))
+        print("Your selections were: the {} and the {}.".format(self.coordinate1.name, self.coordinate2.name))
 
     def check(self):
-        self.selection1.split(" of ")
-        self.selection2.split(" of ")
-        if self.selection1[0] == self.selection2[0]:
+        self.coordinate1.name.split(" of ") #['2','Spades']
+        self.coordinate2.name.split(" of ")
+        if self.coordinate1.name[0] == self.coordinate2.name[0]:
             self.match = True
             return True
         else:
+            self.match = False
             return False
 
     def memory(self):
         if self.match == True:
-            print("Congrats! {} and {} are pairs!".format(self.coordinate1, self.coordinate2))
+            print("Congrats! {} and {} are pairs!".format(self.coordinate1.name, self.coordinate2.name))
+            print("your previous selections were:")
+            for line in self.memoryList:
+                print(line)
             print("\n")
         else:
-            previous = "{} at {}, {} at {}".format(self.selection1, self.coordinate1, self.selection2, self.coordinate2)
+            previous = "{} at {}, {} at {}".format(self.coordinate1.name, self.coordinate1.coordinates, self.coordinate2.name, self.coordinate2.coordinates)
             self.memoryList.append(previous)
-            print("Unfortunately, {} and {} are not pairs.\n".format(self.coordinate1, self.coordinate2))
+            print("Unfortunately, {} and {} are not pairs.\n".format(self.coordinate1.coordinates, self.coordinate2.coordinates))
             print("Try again, your previous selections were:")
             for line in self.memoryList:
                 print(line)
             print("\n")
 
+    def cheat(self, deck_of_cards):
+        layout = deck_of_cards.deckGrid()
+
+
+        self.skip = True
+
     def run(self):
         self.startScreen()
         deck_of_cards = Deck()
         print("Initializing...")
-        deck_of_cards.shuffle()
-        inp = input("The cards has been shuffled once.\nWould you like to shuffle the cards again? (Y/N) \n>>>")
-        if inp.lower() == "y":
-            deck_of_cards.shuffle()
-            print("Shuffling...\nShuffling complete! \nThe game is starting now!")
-            print("\n")
-            board = Board()
-        else:
-            print("Alright then, the game is starting now!")
-            print("\n")
-            board = Board()
-        board.show()
-        while self.end == False:
-            self.makeSelection(deck_of_cards, board)
-            self.check()
-            if self.match == True:
-                board.progress(self.match,self.numrow1,self.numcol1)
-                board.progress(self.match,self.numrow2,self.numcol2)
-                self.memory()
-            else:
-                board.progress(self.match,self.numrow1,self.numcol1)
-                self.memory()
-
-            self.gameComplete(board)
-        if self.end == True:
-            board.progress(self.match,self.numrow1,self.numcol1)
-            print("Congratulations! You have won the game!")
-            print("Exiting...")
-
-    def test(self):
-        self.startScreen
-        deck_of_cards = Deck()
-        print("Initializing...")
+        #deck_of_cards.shuffle()
         inp = input("The cards has not been shuffled.\nWould you like to shuffle the cards? (Y/N) \n>>>")
         if inp.lower() == "y":
             deck_of_cards.shuffle()
@@ -234,22 +218,27 @@ class Game():
             board = Board()
         board.show()
         while self.end == False:
-            self.makeSelection(deck_of_cards, board)
+            self.Select(deck_of_cards, board)
             self.check()
             if self.match == True:
                 board.progress(self.match,self.numrow1,self.numcol1)
                 board.progress(self.match,self.numrow2,self.numcol2)
+                self.memory()
             else:
                 board.progress(self.match,self.numrow1,self.numcol1)
+                self.memory()
 
             self.gameComplete(board)
+
         if self.end == True:
             board.progress(self.match,self.numrow1,self.numcol1)
             print("Congratulations! You have won the game!")
             print("Exiting...")
 
-
 game = Game()
 game.run()
-#game.test()
+
+# test = Deck()
+# test.deckGrid()
+# test.show()
 
